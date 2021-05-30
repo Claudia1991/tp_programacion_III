e648ec666f2dfafdea014d12dee46982ebd24bb7
Claudia Caceres <ccaceres@hexacta.com>
2021-05-30 10:51:40 -0300

    Sprint 2: encuestas

--- app/controllers/EncuestaController.php
+++ app/controllers/EncuestaController.php
@@ -10,26 +10,38 @@ class EncuestaController extends Encuesta implements IApiUsable
       /**
        * Logica Negocio: la mesa tiene que tener estado 4 - cerrada.
        */
-        $parametros = $request->getParsedBody()["body"];
+        $parametros = $request->getParsedBody();
         $puntuacion_descripcion = $parametros['puntuacion_descripcion'];
-        $puntuacion_numero = $parametros['puntuacion_numero'];
+        $puntuacion_mesa = $parametros['puntuacion_mesa'];
+        $puntuacion_restaurante = $parametros['puntuacion_restaurante'];
+        $puntuacion_mozo = $parametros['puntuacion_mozo'];
+        $puntuacion_cocinero = $parametros['puntuacion_cocinero'];
         $codigo_cliente = $parametros['codigo_cliente'];
         $payload =  null;
-        if(!isset($parametros) || !isset($codigo_cliente) || !isset($puntuacion_descripcion) || !isset($puntuacion_numero)){
+        if(!isset($parametros) || !isset($codigo_cliente) || !isset($puntuacion_descripcion) || !isset($puntuacion_mesa) || !isset($puntuacion_restaurante) 
+        || !isset($puntuacion_mozo) || !isset($puntuacion_cocinero)){
           $payload = json_encode(array("error" => "Error en los parametros para crear la encuesta."));
           $response = $response->withStatus(400);
         }else{
           $mesa = Mesa::obtenerMesaSegunCodigoCliente($codigo_cliente);
           if(!$mesa || $mesa->codigo_estado_mesa != 4){
-            $payload = json_encode(array("error" => "No existe la mesa para hacer la encuesta."));
+            $payload = json_encode(array("error" => "No existe la mesa para hacer la encuesta o no esta cerrada."));
             $response = $response->withStatus(400);
-          }elseif($puntuacion_numero < 1 || $puntuacion_numero > 10){
-            $payload = json_encode(array("error" => "Error en el numero de puntuacion."));
+          }elseif(($puntuacion_mesa < 1 || $puntuacion_mesa > 10) || ($puntuacion_restaurante < 1 || $puntuacion_restaurante > 10) 
+          || ($puntuacion_mozo < 1 || $puntuacion_mozo > 10) || ($puntuacion_cocinero < 1 || $puntuacion_cocinero > 10)){
+            $payload = json_encode(array("error" => "Error en el numero de puntuacion. Tiene que ser de 1 a 10."));
+            $response = $response->withStatus(400);
+          }elseif(strlen($puntuacion_descripcion) > 67){
+            $payload = json_encode(array("error" => "La descripcion de la puntuacion es muy larga, como maximo 66 caracteres."));
             $response = $response->withStatus(400);
           }else{
             $encuesta = new Encuesta();
+            $encuesta->id_mesa = $mesa->id;
             $encuesta->puntuacion_descripcion = $puntuacion_descripcion;
-            $encuesta->puntuacion_numero = $puntuacion_numero;
+            $encuesta->puntuacion_cocinero = $puntuacion_cocinero;
+            $encuesta->puntuacion_mozo = $puntuacion_mozo;
+            $encuesta->puntuacion_mesa = $puntuacion_mesa;
+            $encuesta->puntuacion_restaurante = $puntuacion_restaurante;
             $encuesta->codigo_cliente = $codigo_cliente;
             $encuesta->crearEncuesta();
             $payload = json_encode(array("mensaje" => "Encuesta cargada."));
--- app/models/Encuesta.php
+++ app/models/Encuesta.php
@@ -3,18 +3,28 @@
 class Encuesta
 {
     public $id;
-    public $puntuacion_descripcion;
-    public $puntuacion_numero;
+    public $id_mesa;
     public $codigo_cliente;
+    public $puntuacion_descripcion;
+    public $puntuacion_mesa;
+    public $puntuacion_restaurante;
+    public $puntuacion_mozo;
+    public $puntuacion_cocinero;
     public $fecha_hora;
 
     public function crearEncuesta()
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("INSERT INTO Encuesta (puntuacion_descripcion, puntuacion_numero, codigo_cliente,fecha_hora) VALUES (:puntuacion_descripcion, :puntuacion_numero, :codigo_cliente,:fecha_hora)");
+        $consulta = $objAccesoDatos->prepararConsulta("INSERT INTO Encuesta (puntuacion_descripcion, puntuacion_mozo, puntuacion_mesa, puntuacion_cocinero,
+        puntuacion_restaurante,id_mesa, codigo_cliente,fecha_hora) VALUES (:puntuacion_descripcion, :puntuacion_mozo, :puntuacion_mesa, :puntuacion_cocinero,
+        :puntuacion_restaurante, :id_mesa, :codigo_cliente, :fecha_hora)");
         $consulta->bindValue(':puntuacion_descripcion', $this->puntuacion_descripcion, PDO::PARAM_STR);
-        $consulta->bindValue(':puntuacion_numero', $this->puntuacion_numero, PDO::PARAM_INT);
+        $consulta->bindValue(':puntuacion_mozo', $this->puntuacion_mozo, PDO::PARAM_INT);
+        $consulta->bindValue(':puntuacion_mesa', $this->puntuacion_mesa, PDO::PARAM_INT);
+        $consulta->bindValue(':puntuacion_cocinero', $this->puntuacion_cocinero, PDO::PARAM_INT);
+        $consulta->bindValue(':puntuacion_restaurante', $this->puntuacion_restaurante, PDO::PARAM_INT);
         $consulta->bindValue(':codigo_cliente', $this->codigo_cliente, PDO::PARAM_STR);
+        $consulta->bindValue(':id_mesa', $this->id_mesa, PDO::PARAM_INT);
         $consulta->bindValue(':fecha_hora', date('Y-m-d H:i'), PDO::PARAM_INT);
         $consulta->execute();
 
